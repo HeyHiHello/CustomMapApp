df1e9b0de3392d2f5dc7784061b69ed308ddb2ab
HeyHiHello <kirillzazulin@gmail.com>
2024-07-09 22:54:55 +0500

    Node

---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Map.Entry;
+import java.util.Objects;
+
+public class Node<K, V> implements Entry<K, V> {
+    private K key;
+    private V value;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        return this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?, ?> node = (Node<?, ?>) o;
+        return Objects.equals(key, node.key) && Objects.equals(value, node.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+}
